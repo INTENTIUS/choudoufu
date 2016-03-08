psytale/terraform

Added AWS KMS key & alias support

builtin/providers/aws/config.go
@@ -19,6 +19,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/kinesis"
+	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/aws/aws-sdk-go/service/route53"
@@ -57,6 +58,7 @@ type AWSClient struct {
 	kinesisconn     *kinesis.Kinesis
 	elasticacheconn *elasticache.ElastiCache
 	lambdaconn      *lambda.Lambda
+	kmsconn         *kms.KMS
 }
 
 // Client configures and returns a fully initialized AWSClient
@@ -156,6 +158,9 @@ func (c *Config) Client() (interface{}, error) {
 
 		log.Println("[INFO] Initializing CloudWatch SDK connection")
 		client.cloudwatchconn = cloudwatch.New(awsConfig)
+
+		log.Println("[INFO] Initializing KMS connection")
+		client.kmsconn = kms.New(awsConfig)
 	}
 
 	if len(errs) > 0 {

INTENTIUS/choudoufu

builtin/providers/aws/config.go
@@ -42,6 +42,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/glacier"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/kinesis"
+	"github.com/aws/aws-sdk-go/service/kms"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/aws/aws-sdk-go/service/opsworks"
 	"github.com/aws/aws-sdk-go/service/rds"
@@ -97,6 +98,7 @@ type AWSClient struct {
 	rdsconn              *rds.RDS
 	iamconn              *iam.IAM
 	kinesisconn          *kinesis.Kinesis
+	kmsconn              *kms.KMS
 	firehoseconn         *firehose.Firehose
 	elasticacheconn      *elasticache.ElastiCache
 	elasticbeanstalkconn *elasticbeanstalk.ElasticBeanstalk
@@ -294,6 +296,8 @@ func (c *Config) Client() (interface{}, error) {
 		log.Println("[INFO] Initializing Redshift SDK connection")
 		client.redshiftconn = redshift.New(sess)
 
+		log.Println("[INFO] Initializing KMS connection")
+		client.kmsconn = kms.New(sess)
 	}
 
 	if len(errs) > 0 {