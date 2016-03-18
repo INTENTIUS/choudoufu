wjimenez5271/terraform

provider/triton: New provider for Joyent Triton
This brings across the following resources for Triton from the
joyent/triton-terraform repository, and converts them to the canonical
Terraform style, introducing Terraform-style documentation and
acceptance tests which run against the live API rather than the local
APIs:

- triton_firewall_rule
- triton_machine
- triton_key

builtin/providers/triton/provider.go
@@ -1,0 +1,130 @@
+package triton
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/joyent/gocommon/client"
+	"github.com/joyent/gosdc/cloudapi"
+	"github.com/joyent/gosign/auth"
+)
+
+// Provider returns a terraform.ResourceProvider.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"account": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_ACCOUNT", ""),
+			},
+
+			"url": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_URL", "https://us-west-1.api.joyentcloud.com"),
+			},
+
+			"key_path": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_KEY_PATH", ""),
+			},
+
+			"key_material": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_KEY_MATERIAL", ""),
+			},
+
+			"key_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_KEY_ID", ""),
+			},
+		},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"triton_firewall_rule": resourceFirewallRule(),
+			"triton_machine":       resourceMachine(),
+			"triton_key":           resourceKey(),
+		},
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+type SDCConfig struct {
+	Account string
+	KeyPath string
+	KeyID   string
+	URL     string
+}
+
+func (c SDCConfig) validate() error {
+	var err *multierror.Error
+
+	if c.URL == "" {
+		err = multierror.Append(err, fmt.Errorf("URL must be configured for the Triton provider"))
+	}
+	if c.KeyPath == "" {
+		err = multierror.Append(err, fmt.Errorf("Key Path must be configured for the Triton provider"))
+	}
+	if c.KeyID == "" {
+		err = multierror.Append(err, fmt.Errorf("Key ID must be configured for the Triton provider"))
+	}
+	if c.Account == "" {
+		err = multierror.Append(err, fmt.Errorf("Account must be configured for the Triton provider"))
+	}
+
+	return err.ErrorOrNil()
+}
+
+func (c SDCConfig) getSDCClient() (*cloudapi.Client, error) {
+	keyData, err := ioutil.ReadFile(c.KeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	userauth, err := auth.NewAuth(c.Account, string(keyData), "rsa-sha256")
+	if err != nil {
+		return nil, err
+	}
+
+	creds := &auth.Credentials{
+		UserAuthentication: userauth,
+		SdcKeyId:           c.KeyID,
+		SdcEndpoint:        auth.Endpoint{URL: c.URL},
+	}
+
+	client := cloudapi.New(client.NewClient(
+		c.URL,
+		cloudapi.DefaultAPIVersion,
+		creds,
+		&cloudapi.Logger,
+	))
+
+	return client, nil
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := SDCConfig{
+		Account: d.Get("account").(string),
+		URL:     d.Get("url").(string),
+		KeyPath: d.Get("key_path").(string),
+		KeyID:   d.Get("key_id").(string),
+	}
+
+	if err := config.validate(); err != nil {
+		return nil, err
+	}
+
+	client, err := config.getSDCClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}

builtin/providers/triton/provider_test.go
@@ -1,0 +1,44 @@
+package triton
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider().(*schema.Provider)
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"triton": testAccProvider,
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	sdcURL := os.Getenv("SDC_URL")
+	account := os.Getenv("SDC_ACCOUNT")
+	keyID := os.Getenv("SDC_KEY_ID")
+	keyPath := os.Getenv("SDC_KEY_PATH")
+
+	if sdcURL == "" {
+		sdcURL = "https://us-west-1.api.joyentcloud.com"
+	}
+
+	if sdcURL == "" || account == "" || keyID == "" || keyPath == "" {
+		t.Fatal("SDC_ACCOUNT, SDC_KEY_ID and SDC_KEY_PATH must be set for acceptance tests")
+	}
+}

INTENTIUS/choudoufu

builtin/providers/triton/provider.go
@@ -1,0 +1,118 @@
+package triton
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/joyent/gocommon/client"
+	"github.com/joyent/gosdc/cloudapi"
+	"github.com/joyent/gosign/auth"
+)
+
+// Provider returns a terraform.ResourceProvider.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"account": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_ACCOUNT", ""),
+			},
+
+			"url": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_URL", "https://us-west-1.api.joyentcloud.com"),
+			},
+
+			"key_material": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_KEY_MATERIAL", ""),
+			},
+
+			"key_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_KEY_ID", ""),
+			},
+		},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"triton_firewall_rule": resourceFirewallRule(),
+			"triton_machine":       resourceMachine(),
+			"triton_key":           resourceKey(),
+		},
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+type SDCConfig struct {
+	Account     string
+	KeyMaterial string
+	KeyID       string
+	URL         string
+}
+
+func (c SDCConfig) validate() error {
+	var err *multierror.Error
+
+	if c.URL == "" {
+		err = multierror.Append(err, fmt.Errorf("URL must be configured for the Triton provider"))
+	}
+	if c.KeyMaterial == "" {
+		err = multierror.Append(err, fmt.Errorf("Key Material must be configured for the Triton provider"))
+	}
+	if c.KeyID == "" {
+		err = multierror.Append(err, fmt.Errorf("Key ID must be configured for the Triton provider"))
+	}
+	if c.Account == "" {
+		err = multierror.Append(err, fmt.Errorf("Account must be configured for the Triton provider"))
+	}
+
+	return err.ErrorOrNil()
+}
+
+func (c SDCConfig) getSDCClient() (*cloudapi.Client, error) {
+	userauth, err := auth.NewAuth(c.Account, c.KeyMaterial, "rsa-sha256")
+	if err != nil {
+		return nil, err
+	}
+
+	creds := &auth.Credentials{
+		UserAuthentication: userauth,
+		SdcKeyId:           c.KeyID,
+		SdcEndpoint:        auth.Endpoint{URL: c.URL},
+	}
+
+	client := cloudapi.New(client.NewClient(
+		c.URL,
+		cloudapi.DefaultAPIVersion,
+		creds,
+		&cloudapi.Logger,
+	))
+
+	return client, nil
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := SDCConfig{
+		Account:     d.Get("account").(string),
+		URL:         d.Get("url").(string),
+		KeyMaterial: d.Get("key_material").(string),
+		KeyID:       d.Get("key_id").(string),
+	}
+
+	if err := config.validate(); err != nil {
+		return nil, err
+	}
+
+	client, err := config.getSDCClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}

builtin/providers/triton/provider_test.go
@@ -1,0 +1,44 @@
+package triton
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider().(*schema.Provider)
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"triton": testAccProvider,
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	sdcURL := os.Getenv("SDC_URL")
+	account := os.Getenv("SDC_ACCOUNT")
+	keyID := os.Getenv("SDC_KEY_ID")
+	keyMaterial := os.Getenv("SDC_KEY_MATERIAL")
+
+	if sdcURL == "" {
+		sdcURL = "https://us-west-1.api.joyentcloud.com"
+	}
+
+	if sdcURL == "" || account == "" || keyID == "" || keyMaterial == "" {
+		t.Fatal("SDC_ACCOUNT, SDC_KEY_ID and SDC_KEY_MATERIAL must be set for acceptance tests")
+	}
+}