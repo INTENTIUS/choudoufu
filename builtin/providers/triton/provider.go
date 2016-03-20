wjimenez5271/terraform

provider/triton: New provider for Joyent Triton
This brings across the following resources for Triton from the
joyent/triton-terraform repository, and converts them to the canonical
Terraform style, introducing Terraform-style documentation and
acceptance tests which run against the live API rather than the local
APIs:

- triton_firewall_rule
- triton_machine
- triton_key

builtin/providers/triton/provider.go
@@ -1,0 +1,130 @@
+package triton
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/joyent/gocommon/client"
+	"github.com/joyent/gosdc/cloudapi"
+	"github.com/joyent/gosign/auth"
+)
+
+// Provider returns a terraform.ResourceProvider.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"account": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_ACCOUNT", ""),
+			},
+
+			"url": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_URL", "https://us-west-1.api.joyentcloud.com"),
+			},
+
+			"key_path": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_KEY_PATH", ""),
+			},
+
+			"key_material": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_KEY_MATERIAL", ""),
+			},
+
+			"key_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_KEY_ID", ""),
+			},
+		},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"triton_firewall_rule": resourceFirewallRule(),
+			"triton_machine":       resourceMachine(),
+			"triton_key":           resourceKey(),
+		},
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+type SDCConfig struct {
+	Account string
+	KeyPath string
+	KeyID   string
+	URL     string
+}
+
+func (c SDCConfig) validate() error {
+	var err *multierror.Error
+
+	if c.URL == "" {
+		err = multierror.Append(err, fmt.Errorf("URL must be configured for the Triton provider"))
+	}
+	if c.KeyPath == "" {
+		err = multierror.Append(err, fmt.Errorf("Key Path must be configured for the Triton provider"))
+	}
+	if c.KeyID == "" {
+		err = multierror.Append(err, fmt.Errorf("Key ID must be configured for the Triton provider"))
+	}
+	if c.Account == "" {
+		err = multierror.Append(err, fmt.Errorf("Account must be configured for the Triton provider"))
+	}
+
+	return err.ErrorOrNil()
+}
+
+func (c SDCConfig) getSDCClient() (*cloudapi.Client, error) {
+	keyData, err := ioutil.ReadFile(c.KeyPath)
+	if err != nil {
+		return nil, err
+	}
+
+	userauth, err := auth.NewAuth(c.Account, string(keyData), "rsa-sha256")
+	if err != nil {
+		return nil, err
+	}
+
+	creds := &auth.Credentials{
+		UserAuthentication: userauth,
+		SdcKeyId:           c.KeyID,
+		SdcEndpoint:        auth.Endpoint{URL: c.URL},
+	}
+
+	client := cloudapi.New(client.NewClient(
+		c.URL,
+		cloudapi.DefaultAPIVersion,
+		creds,
+		&cloudapi.Logger,
+	))
+
+	return client, nil
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := SDCConfig{
+		Account: d.Get("account").(string),
+		URL:     d.Get("url").(string),
+		KeyPath: d.Get("key_path").(string),
+		KeyID:   d.Get("key_id").(string),
+	}
+
+	if err := config.validate(); err != nil {
+		return nil, err
+	}
+
+	client, err := config.getSDCClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}

vendor/github.com/joyent/gocommon/client/client.go
@@ -1,0 +1,111 @@
+//
+// gocommon - Go library to interact with the JoyentCloud
+//
+//
+// Copyright (c) 2013 Joyent Inc.
+//
+// Written by Daniele Stroppa <daniele.stroppa@joyent.com>
+//
+
+package client
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/juju/loggo"
+
+	joyenthttp "github.com/joyent/gocommon/http"
+	"github.com/joyent/gosign/auth"
+)
+
+const (
+	// The HTTP request methods.
+	GET    = "GET"
+	POST   = "POST"
+	PUT    = "PUT"
+	DELETE = "DELETE"
+	HEAD   = "HEAD"
+	COPY   = "COPY"
+)
+
+// Client implementations sends service requests to the JoyentCloud.
+type Client interface {
+	SendRequest(method, apiCall, rfc1123Date string, request *joyenthttp.RequestData, response *joyenthttp.ResponseData) (err error)
+	// MakeServiceURL prepares a full URL to a service endpoint, with optional
+	// URL parts. It uses the first endpoint it can find for the given service type.
+	MakeServiceURL(parts []string) string
+	SignURL(path string, expires time.Time) (string, error)
+}
+
+// This client sends requests without authenticating.
+type client struct {
+	mu         sync.Mutex
+	logger     *loggo.Logger
+	baseURL    string
+	creds      *auth.Credentials
+	httpClient *joyenthttp.Client
+}
+
+var _ Client = (*client)(nil)
+
+func newClient(baseURL string, credentials *auth.Credentials, httpClient *joyenthttp.Client, logger *loggo.Logger) Client {
+	client := client{baseURL: baseURL, logger: logger, creds: credentials, httpClient: httpClient}
+	return &client
+}
+
+func NewClient(baseURL, apiVersion string, credentials *auth.Credentials, logger *loggo.Logger) Client {
+	sharedHttpClient := joyenthttp.New(credentials, apiVersion, logger)
+	return newClient(baseURL, credentials, sharedHttpClient, logger)
+}
+
+func (c *client) sendRequest(method, url, rfc1123Date string, request *joyenthttp.RequestData, response *joyenthttp.ResponseData) (err error) {
+	if request.ReqValue != nil || response.RespValue != nil {
+		err = c.httpClient.JsonRequest(method, url, rfc1123Date, request, response)
+	} else {
+		err = c.httpClient.BinaryRequest(method, url, rfc1123Date, request, response)
+	}
+	return
+}
+
+func (c *client) SendRequest(method, apiCall, rfc1123Date string, request *joyenthttp.RequestData, response *joyenthttp.ResponseData) (err error) {
+	url := c.MakeServiceURL([]string{c.creds.UserAuthentication.User, apiCall})
+	err = c.sendRequest(method, url, rfc1123Date, request, response)
+	return
+}
+
+func makeURL(base string, parts []string) string {
+	if !strings.HasSuffix(base, "/") && len(parts) > 0 {
+		base += "/"
+	}
+	if parts[1] == "" {
+		return base + parts[0]
+	}
+	return base + strings.Join(parts, "/")
+}
+
+func (c *client) MakeServiceURL(parts []string) string {
+	return makeURL(c.baseURL, parts)
+}
+
+func (c *client) SignURL(path string, expires time.Time) (string, error) {
+	parsedURL, err := url.Parse(c.baseURL)
+	if err != nil {
+		return "", fmt.Errorf("bad Manta endpoint URL %q: %v", c.baseURL, err)
+	}
+	userAuthentication := c.creds.UserAuthentication
+	userAuthentication.Algorithm = "RSA-SHA1"
+	keyId := url.QueryEscape(fmt.Sprintf("/%s/keys/%s", userAuthentication.User, c.creds.MantaKeyId))
+	params := fmt.Sprintf("algorithm=%s&expires=%d&keyId=%s", userAuthentication.Algorithm, expires.Unix(), keyId)
+	signingLine := fmt.Sprintf("GET\n%s\n%s\n%s", parsedURL.Host, path, params)
+
+	signature, err := auth.GetSignature(userAuthentication, signingLine)
+	if err != nil {
+		return "", fmt.Errorf("cannot generate URL signature: %v", err)
+	}
+	signedURL := fmt.Sprintf("%s%s?%s&signature=%s", c.baseURL, path, params, url.QueryEscape(signature))
+	return signedURL, nil
+}

INTENTIUS/choudoufu

builtin/providers/triton/provider.go
@@ -1,0 +1,120 @@
+package triton
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/joyent/gocommon/client"
+	"github.com/joyent/gosdc/cloudapi"
+	"github.com/joyent/gosign/auth"
+)
+
+// Provider returns a terraform.ResourceProvider.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"account": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_ACCOUNT", ""),
+			},
+
+			"url": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_URL", "https://us-west-1.api.joyentcloud.com"),
+			},
+
+			"key_material": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_KEY_MATERIAL", ""),
+			},
+
+			"key_id": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SDC_KEY_ID", ""),
+			},
+		},
+
+		ResourcesMap: map[string]*schema.Resource{
+			"triton_firewall_rule": resourceFirewallRule(),
+			"triton_machine":       resourceMachine(),
+			"triton_key":           resourceKey(),
+		},
+		ConfigureFunc: providerConfigure,
+	}
+}
+
+type SDCConfig struct {
+	Account     string
+	KeyMaterial string
+	KeyID       string
+	URL         string
+}
+
+func (c SDCConfig) validate() error {
+	var err *multierror.Error
+
+	if c.URL == "" {
+		err = multierror.Append(err, fmt.Errorf("URL must be configured for the Triton provider"))
+	}
+	if c.KeyMaterial == "" {
+		err = multierror.Append(err, fmt.Errorf("Key Material must be configured for the Triton provider"))
+	}
+	if c.KeyID == "" {
+		err = multierror.Append(err, fmt.Errorf("Key ID must be configured for the Triton provider"))
+	}
+	if c.Account == "" {
+		err = multierror.Append(err, fmt.Errorf("Account must be configured for the Triton provider"))
+	}
+
+	return err.ErrorOrNil()
+}
+
+func (c SDCConfig) getSDCClient() (*cloudapi.Client, error) {
+	userauth, err := auth.NewAuth(c.Account, c.KeyMaterial, "rsa-sha256")
+	if err != nil {
+		return nil, err
+	}
+
+	creds := &auth.Credentials{
+		UserAuthentication: userauth,
+		SdcKeyId:           c.KeyID,
+		SdcEndpoint:        auth.Endpoint{URL: c.URL},
+	}
+
+	client := cloudapi.New(client.NewClient(
+		c.URL,
+		cloudapi.DefaultAPIVersion,
+		creds,
+		log.New(os.Stderr, "", log.LstdFlags),
+	))
+
+	return client, nil
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	config := SDCConfig{
+		Account:     d.Get("account").(string),
+		URL:         d.Get("url").(string),
+		KeyMaterial: d.Get("key_material").(string),
+		KeyID:       d.Get("key_id").(string),
+	}
+
+	if err := config.validate(); err != nil {
+		return nil, err
+	}
+
+	client, err := config.getSDCClient()
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}

vendor/github.com/joyent/gocommon/client/client.go
@@ -1,0 +1,110 @@
+//
+// gocommon - Go library to interact with the JoyentCloud
+//
+//
+// Copyright (c) 2013 Joyent Inc.
+//
+// Written by Daniele Stroppa <daniele.stroppa@joyent.com>
+//
+
+package client
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"strings"
+	"sync"
+	"time"
+
+	joyenthttp "github.com/joyent/gocommon/http"
+	"github.com/joyent/gosign/auth"
+)
+
+const (
+	// The HTTP request methods.
+	GET    = "GET"
+	POST   = "POST"
+	PUT    = "PUT"
+	DELETE = "DELETE"
+	HEAD   = "HEAD"
+	COPY   = "COPY"
+)
+
+// Client implementations sends service requests to the JoyentCloud.
+type Client interface {
+	SendRequest(method, apiCall, rfc1123Date string, request *joyenthttp.RequestData, response *joyenthttp.ResponseData) (err error)
+	// MakeServiceURL prepares a full URL to a service endpoint, with optional
+	// URL parts. It uses the first endpoint it can find for the given service type.
+	MakeServiceURL(parts []string) string
+	SignURL(path string, expires time.Time) (string, error)
+}
+
+// This client sends requests without authenticating.
+type client struct {
+	mu         sync.Mutex
+	logger     *log.Logger
+	baseURL    string
+	creds      *auth.Credentials
+	httpClient *joyenthttp.Client
+}
+
+var _ Client = (*client)(nil)
+
+func newClient(baseURL string, credentials *auth.Credentials, httpClient *joyenthttp.Client, logger *log.Logger) Client {
+	client := client{baseURL: baseURL, logger: logger, creds: credentials, httpClient: httpClient}
+	return &client
+}
+
+func NewClient(baseURL, apiVersion string, credentials *auth.Credentials, logger *log.Logger) Client {
+	sharedHttpClient := joyenthttp.New(credentials, apiVersion, logger)
+	return newClient(baseURL, credentials, sharedHttpClient, logger)
+}
+
+func (c *client) sendRequest(method, url, rfc1123Date string, request *joyenthttp.RequestData, response *joyenthttp.ResponseData) (err error) {
+	if request.ReqValue != nil || response.RespValue != nil {
+		err = c.httpClient.JsonRequest(method, url, rfc1123Date, request, response)
+	} else {
+		err = c.httpClient.BinaryRequest(method, url, rfc1123Date, request, response)
+	}
+	return
+}
+
+func (c *client) SendRequest(method, apiCall, rfc1123Date string, request *joyenthttp.RequestData, response *joyenthttp.ResponseData) (err error) {
+	url := c.MakeServiceURL([]string{c.creds.UserAuthentication.User, apiCall})
+	err = c.sendRequest(method, url, rfc1123Date, request, response)
+	return
+}
+
+func makeURL(base string, parts []string) string {
+	if !strings.HasSuffix(base, "/") && len(parts) > 0 {
+		base += "/"
+	}
+	if parts[1] == "" {
+		return base + parts[0]
+	}
+	return base + strings.Join(parts, "/")
+}
+
+func (c *client) MakeServiceURL(parts []string) string {
+	return makeURL(c.baseURL, parts)
+}
+
+func (c *client) SignURL(path string, expires time.Time) (string, error) {
+	parsedURL, err := url.Parse(c.baseURL)
+	if err != nil {
+		return "", fmt.Errorf("bad Manta endpoint URL %q: %v", c.baseURL, err)
+	}
+	userAuthentication := c.creds.UserAuthentication
+	userAuthentication.Algorithm = "RSA-SHA1"
+	keyId := url.QueryEscape(fmt.Sprintf("/%s/keys/%s", userAuthentication.User, c.creds.MantaKeyId))
+	params := fmt.Sprintf("algorithm=%s&expires=%d&keyId=%s", userAuthentication.Algorithm, expires.Unix(), keyId)
+	signingLine := fmt.Sprintf("GET\n%s\n%s\n%s", parsedURL.Host, path, params)
+
+	signature, err := auth.GetSignature(userAuthentication, signingLine)
+	if err != nil {
+		return "", fmt.Errorf("cannot generate URL signature: %v", err)
+	}
+	signedURL := fmt.Sprintf("%s%s?%s&signature=%s", c.baseURL, path, params, url.QueryEscape(signature))
+	return signedURL, nil
+}