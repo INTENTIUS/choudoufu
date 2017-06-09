meteor/terraform

core: allow overriding environment name via env var
This allows you to run multiple concurrent terraform operations against
different environments from the same source directory.

Fixes #14447.

Also removes some dead code which appears to do the same thing as the function I
modified.

command/env_list.go
@@ -39,7 +39,7 @@ func (c *EnvListCommand) Run(args []string) int {
 		return 1
 	}
 
-	env := c.Env()
+	env, isOverridden := c.EnvWithIsOverridden()
 
 	var out bytes.Buffer
 	for _, s := range states {
@@ -52,6 +52,11 @@ func (c *EnvListCommand) Run(args []string) int {
 	}
 
 	c.Ui.Output(out.String())
+
+	if isOverridden {
+		c.Ui.Output(envIsOverriddenNote)
+	}
+
 	return 0
 }
 

INTENTIUS/choudoufu

command/env_list.go
@@ -48,7 +48,7 @@ func (c *EnvListCommand) Run(args []string) int {
 		return 1
 	}
 
-	env := c.Env()
+	env, isOverridden := c.EnvOverridden()
 
 	var out bytes.Buffer
 	for _, s := range states {
@@ -61,6 +61,11 @@ func (c *EnvListCommand) Run(args []string) int {
 	}
 
 	c.Ui.Output(out.String())
+
+	if isOverridden {
+		c.Ui.Output(envIsOverriddenNote)
+	}
+
 	return 0
 }
 